d3120d0b9311fdc75584b739b34f84ed84508afd
MacRat <m@crat.jp>
2021-03-21 22:45:05 +0900

    feat: support set zero to initial time

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ class TimeButton {
     constructor(labelSelector, radioSelector, initial) {
         this.label = document.querySelector(labelSelector);
         this.radio = document.querySelector(radioSelector);
-        this.remain = initial ? Number(initial) : (60 * 60 * 1000);
+        this.remain = this.initial = Number(initial);
     }
 
     update(delta) {
@@ -30,7 +30,7 @@ class TimeButton {
 
         this.label.innerText = second2str(Math.round(this.remain / 1000));
 
-        if (this.remain < 0) {
+        if (this.remain < 0 && this.initial > 0) {
             this.label.classList.add('timeup');
         } else {
             this.label.classList.remove('timeup');
@@ -39,19 +39,15 @@ class TimeButton {
 }
 
 
-const whiteInitial = localStorage.getItem('white-initial');
-const blackInitial = localStorage.getItem('black-initial');
+const whiteInitial = localStorage.getItem('white-initial') ? Number(localStorage.getItem('white-initial')) : (60 * 60 * 1000);
+const blackInitial = localStorage.getItem('black-initial') ? Number(localStorage.getItem('black-initial')) : (60 * 60 * 1000);
 
 const white = new TimeButton('label.white', '.black.radio', localStorage.getItem('white-remain') || whiteInitial);
 const black = new TimeButton('label.black', '.white.radio', localStorage.getItem('black-remain') || blackInitial);
 const favicon = document.querySelector('#favicon');
 
-if (whiteInitial) {
-    document.querySelector('.white.initial-time').value = Number(whiteInitial) / 60 / 1000;
-}
-if (blackInitial) {
-    document.querySelector('.black.initial-time').value = Number(blackInitial) / 60 / 1000;
-}
+document.querySelector('.white.initial-time').value = whiteInitial / 60 / 1000;
+document.querySelector('.black.initial-time').value = blackInitial / 60 / 1000;
 
 
 let beforeTime = new Date().getTime();
@@ -75,11 +71,11 @@ setInterval(() => {
 
 
 document.querySelector('button').addEventListener('click', () => {
-    white.remain = Number(document.querySelector('.white.initial-time').value) * 60 * 1000;
-    black.remain = Number(document.querySelector('.black.initial-time').value) * 60 * 1000;
+    white.remain = white.initial = Number(document.querySelector('.white.initial-time').value) * 60 * 1000;
+    black.remain = black.initial = Number(document.querySelector('.black.initial-time').value) * 60 * 1000;
 
-    localStorage.setItem('white-initial', white.remain);
-    localStorage.setItem('black-initial', black.remain);
+    localStorage.setItem('white-initial', white.initial);
+    localStorage.setItem('black-initial', black.initial);
 });
 
 
